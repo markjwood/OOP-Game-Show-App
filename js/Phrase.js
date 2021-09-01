5b995ee7be97a508a3f1d308c1c1232525cc20c2
Mark <mark@markjwood.com>
2021-09-01 14:30:35 -0700

    added Phrase() class and addPhraseToDisplay() method

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  /**
+   * adds letter placeholders to display
+   */
+  addPhraseToDisplay() {
+    const charsArray = this.phrase.split('');
+    const phraseUl = document.getElementById('phrase').firstElementChild;
+
+    charsArray.forEach(char => {
+      const li = document.createElement('li');
+      if (char === ' ') {
+        li.className = 'space';
+      } else {
+        li.classList.add('hide', 'letter', char);
+      }
+      li.textContent = char;
+      phraseUl.appendChild(li);
+    });
+  }
+}
