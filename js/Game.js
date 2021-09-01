b20903f7c55480337dcdc73bf54577054aa5709e
Mark <mark@markjwood.com>
2021-09-01 14:32:39 -0700

    added Game() class and createPhrases() method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+  }
+
+  /**
+   * Creates phrases for use in game
+   * @returns {array} An array of phrase objects
+   */
+  createPhrases() {
+    const phrases = [
+      'speak of the devil',
+      'time flies when you are having fun',
+      'a penny for your thoughts',
+      'every cloud has a silver lining',
+      'kill two birds with one stone'
+    ];
+    const phraseObjects = [];
+    phrases.forEach(phrase => phraseObjects.push(new Phrase(phrase)));
+    return phraseObjects;
+  }
+}
