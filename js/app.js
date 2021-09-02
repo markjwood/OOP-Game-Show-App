ba27ffac5cd23b0d1d1887fe3846865b1e433557
Mark <mark@markjwood.com>
2021-09-02 09:53:12 -0700

    added click event listener on the start button to call startGame() method

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+let game;
+const startButton = document.getElementById('btn__reset');
+
+startButton.addEventListener('click', () => {
+  game = new Game();
+  game.startGame();
+});
